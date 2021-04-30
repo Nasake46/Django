08d7a7f07d20f247f582a3a73f82f93754ef855d
Nasake46 <wilfrid.salles-otta@supinternet.fr>
2021-04-30 15:26:48 +0200

    Changements yeah

--- paste/models.py
+++ paste/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Paste(models.Model):
     url = models.CharField(max_length=128)
     content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.url
